76a625baab511ffde60ab9cdd04527a62705fdd3
Cr4zi <ori.mizrahi1000@gmail.com>
2021-01-07 21:59:19 +0200

    importing stuff

---
+++ src/main.py
@@ -0,0 +1,11 @@
+# IMPORT SECTION
+import os
+from dotenv import load_dotenv
+# Loading .env file
+load_dotenv()
+
+from pynput import mouse, keyboard
+
+from googleapiclient.discovery import build
+
+# END IMPORT SECTION
