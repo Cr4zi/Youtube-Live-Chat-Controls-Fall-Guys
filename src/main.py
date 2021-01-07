3507717c79dc826757a024296727e2edb6bbc992
Cr4zi <ori.mizrahi1000@gmail.com>
2021-01-07 22:03:21 +0200

    Making a class called YoutubeLiveChat. defined 2 functions

--- src/main.py
+++ src/main.py
@@ -9,3 +9,18 @@ from pynput import mouse, keyboard
 from googleapiclient.discovery import build
 
 # END IMPORT SECTION
+
+
+class YoutubeLiveChat(object):
+    def __init__(self):
+        self.youtube = build('youtube', 'v3', developerKey=os.getenv('API_KEY'))
+
+    def reading_chat(self):
+        pass
+
+    def move_in_game(self):
+        pass
+
+
+if __name__ == '__main__':
+    ylc = YoutubeLiveChat()
