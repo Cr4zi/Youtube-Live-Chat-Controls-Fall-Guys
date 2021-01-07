62e59ac240e46f186e8408603793674cb084a7de
Cr4zi <ori.mizrahi1000@gmail.com>
2021-01-07 23:07:50 +0200

    Reading the chat messages

--- src/main.py
+++ src/main.py
@@ -4,19 +4,25 @@ from dotenv import load_dotenv
 # Loading .env file
 load_dotenv()
 
-from pynput import mouse, keyboard
+import pytchat
 
-from googleapiclient.discovery import build
+from pynput import mouse, keyboard
 
 # END IMPORT SECTION
 
 
 class YoutubeLiveChat(object):
     def __init__(self):
-        self.youtube = build('youtube', 'v3', developerKey=os.getenv('API_KEY'))
+        self.chat_input = self.reading_chat()
 
     def reading_chat(self):
-        pass
+        chat = pytchat.create(video_id='Yfr1zUw3o1I')
+        while chat.is_alive():
+            for c in chat.get().sync_items():
+                if c.message == 'd' or c.message == 'a' or c.message == 's' or c.message == 'w':
+                    return c.message
+                else:
+                    continue
 
     def move_in_game(self):
         pass
@@ -24,3 +30,4 @@ class YoutubeLiveChat(object):
 
 if __name__ == '__main__':
     ylc = YoutubeLiveChat()
+    ylc.reading_chat()
